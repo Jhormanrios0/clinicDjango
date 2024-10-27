670263aefdc076fd7bf611d4bf89227a6891939b
Jhormanrios0 <jhormanrios02@gmail.com>
2024-10-26 23:30:37 -0500

    fix static files

--- clinicaTendencias/settings.py
+++ clinicaTendencias/settings.py
@@ -122,14 +122,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'myapp/static')]
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
